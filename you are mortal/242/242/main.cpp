d528ed5c895cf5af283860b655aa9939850ba5a6
soupsoup88 <soupsoup8888@gmail.com>
2016-05-28 11:30:56 -0400

    no message

--- you are mortal/242/242/main.cpp
+++ you are mortal/242/242/main.cpp
@@ -13,13 +13,28 @@ using namespace std;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
+        int m = (int)s.length();
+        int n = (int)t.length();
+        bool result;
+        vector<int> vec1(26,0);
+        vector<int> vec2(26,0);
+        int i;
+        if (m!=n){
+            return 0;
+        }
+        for(i = 0;i<m;i++){
+            vec1[s[i]-'a']++;
+            vec2[t[i]-'a']++;
+        }
+        vec1==vec2?result = 1:result = 0;
+        return result;
         
     }
 };
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Solution haha;
+    cout<<haha.isAnagram("car","car")<<endl;
+    cout<<haha.isAnagram("car", "cat")<<endl;
     return 0;
 }
